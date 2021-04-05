594e84b32a431e82c2661630a0d4c699e7c46a13
Trif Adriana <trif.corina.adriana@gmail.com>
2021-04-05 16:15:22 +0300

    csrf token

--- src_ts/api-requests.ts
+++ src_ts/api-requests.ts
@@ -14,9 +14,40 @@ export function changeCountry(countryId: string) {
          { method: 'POST',
            headers: {
             'Accept': 'application/json, text/plain',
-            'Content-Type': 'application/json;charset=UTF-8'
+            'Content-Type': 'application/json;charset=UTF-8',
+            ... getCsrfHeader()
+
            },
            body: JSON.stringify({country: countryId})
         })
     .then(resp => { if (Number(resp.status) >= 400) { throw resp.json() }})
+}
+
+export function getCsrfHeader() {
+    const csrfHeaders = {};
+    const csrfToken = _getCSRFCookie();
+
+    if (csrfToken) {
+      csrfHeaders['x-csrftoken'] = csrfToken;
+    }
+    return csrfHeaders;
+}
+  
+   
+export function _getCSRFCookie() {
+    // check for a csrftoken cookie and return its value
+    const csrfCookieName = 'csrftoken';
+    let csrfToken = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, csrfCookieName.length + 1) === csrfCookieName + '=') {
+            csrfToken = decodeURIComponent(cookie.substring(csrfCookieName.length + 1));
+            break;
+        }
+        }
+    }
+    return csrfToken;
 }
