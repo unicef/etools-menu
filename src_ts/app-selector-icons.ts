5a3b3a6df743d1cbe3984e02fda3c0d9a088217e
Adriana Trif <trif.corina.adriana@gmail.com>
2023-05-25 13:59:45 +0300

    Add AMP link (#22)

--- src_ts/app-selector-icons.ts
+++ src_ts/app-selector-icons.ts
@@ -591,3 +591,19 @@ export const datamartIcon = html`<svg id="datamart" height="45" viewBox="0 0 23 
     <path d="M2 20h20v-4H2v4zm2-3h2v2H4v-2zM2 4v4h20V4H2zm4 3H4V5h2v2zm-4 7h20v-4H2v4zm2-3h2v2H4v-2z"></path>
   </g>
 </svg>`;
+
+export const ampIcon = html`<svg
+  width="36px"
+  height="36px"
+  viewBox="0 0 24 24"
+  fill="#ffffff"
+  preserveAspectRatio="xMidYMid meet"
+  focusable="false"
+  style="background-color:#0099ff"
+>
+  <g>
+    <path
+      d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"
+    ></path>
+  </g>
+</svg>`;
