7c2ae22a4753cd84b3dd285f000637bc7a399cef
Pendea Raul <raul.pendea@nordlogic.com>
2026-02-09 18:03:12 +0200

    CO Viewer access

--- src_ts/UserGroupsEnum.ts
+++ src_ts/UserGroupsEnum.ts
@@ -1,6 +1,7 @@
 export enum UserGroupsEnum {
   LMSM_VIEWER = 'IP LM Viewer',
   LMSM_EDITOR = 'IP LM Editor',
+  LMSM_CO_VIEWER = 'LMSM CO Viewer',
   LMSM_ADMIN = 'LMSM Admin Panel',
   LMSM_ADMIN_CO = 'LMSM CO Admin',
   LMSM_ADMIN_HQ = 'LMSM HQ Admin'
